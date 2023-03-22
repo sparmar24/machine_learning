883ee4285e63eac49372e78aec46c27786fc4fd8
Sonia Parmar <soniaparmar24@gmail.com>
2023-03-22 20:28:29 +0100

    Move cross validation score into for loop

--- modelvalidation_casestudy.py
+++ modelvalidation_casestudy.py
@@ -16,11 +16,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 
-# load the dataset
-dataset = pd.read_csv("Breast_Cancer_Data.csv")
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
 
 def pred_score(ps_estimator, ps_train_X, ps_test_X, ps_train_y, ps_test_y):
     ps_estimator.fit(ps_train_X, ps_train_y)
@@ -32,6 +27,11 @@ def pred_score(ps_estimator, ps_train_X, ps_test_X, ps_train_y, ps_test_y):
 
 
 def main():
+    # load the dataset
+    dataset = pd.read_csv("Breast_Cancer_Data.csv")
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
+
     # split the training and test data
     train_X, test_X, train_y, test_y = train_test_split(
         X, y, test_size=0.25, random_state=40
@@ -61,69 +61,16 @@ def main():
         prediction, confusion, accuracy = pred_score(
             estimator, train_X, test_X, train_y, test_y
         )
-        value = {"prediction": prediction, "confusion": confusion, "accuracy": accuracy}
-        models_and_predictions[f"{estimator_name}"] = value
-
-    breakpoint()
-
-    # training different models on training data
-    """ Logistic Regression """
-    classifier = LogisticRegression(random_state=0)
-    lr_predicted, lr_confusion, lr_accuracy = pred_score(
-        classifier, train_X, test_X, train_y, test_y
-    )
-    """ KNN regression """
-    knnreg = KNeighborsClassifier(n_neighbors=5)
-    knn_predicted, knn_confusion, knn_accuracy = pred_score(
-        knnreg, train_X, test_X, train_y, test_y
-    )
-    """ svm linear regression """
-    svcreg = SVC(kernel="linear", random_state=0)
-    svc_predicted, svc_confusion, svc_accuracy = pred_score(
-        svcreg, train_X, test_X, train_y, test_y
-    )
-    """ Kernel svm regression """
-    svcreg_rbf = SVC(kernel="rbf", random_state=0)
-    rbf_predicted, rbf_confusion, rbf_accuracy = pred_score(
-        svcreg_rbf, train_X, test_X, train_y, test_y
-    )
-    """ Naive Bayes regression """
-    bayes_reg = GaussianNB()
-    b_predicted, b_confusion, b_accuracy = pred_score(
-        bayes_reg, train_X, test_X, train_y, test_y
-    )
-    """ Decision Tree regression """
-    d_reg = DecisionTreeClassifier(criterion="entropy", random_state=0)
-    d_predicted, d_confusion, d_accuracy = pred_score(
-        d_reg, train_X, test_X, train_y, test_y
-    )
-    """ Random Forest regression """
-    rf_reg = RandomForestClassifier(
-        criterion="entropy", n_estimators=5, random_state=0
-    )
-    rf_predicted, rf_confusion, rf_accuracy = pred_score(
-        rf_reg, train_X, test_X, train_y, test_y
-    )
-
-    print("==============================================")
-    breakpoint()
-
-    print("accuracy score after applying k-fold validation (here, k = 10)")
-    score1 = cross_val_score(estimator=classifier, X=train_X, y=train_y, cv=10)
-    score2 = cross_val_score(estimator=knnreg, X=train_X, y=train_y, cv=10)
-    score3 = cross_val_score(estimator=svcreg, X=train_X, y=train_y, cv=10)
-    score4 = cross_val_score(estimator=svcreg_rbf, X=train_X, y=train_y, cv=10)
-    score5 = cross_val_score(estimator=bayes_reg, X=train_X, y=train_y, cv=10)
-    score6 = cross_val_score(estimator=d_reg, X=train_X, y=train_y, cv=10)
-    score7 = cross_val_score(estimator=rf_reg, X=train_X, y=train_y, cv=10)
-    print("==============================================")
-    # print("Logistic Regression acc_score: ", sum(score1) / len(score1))
-    # print("kNN acc_score: ", sum(score2) / len(score2))
-    # print("SVM acc_score: ", sum(score3) / len(score3))
-    # print("SVM 'rbf' acc_score: ", sum(score4) / len(score4))
-    # print("Naive Bayes acc_score: ", sum(score5) / len(score5))
-    # print("Decision Tree acc_score: ", sum(score6) / len(score6))
-    # print("Random Forest acc_score: ", sum(score7) / len(score7))
+        # accuracy score after applying k-fold validation (here, k = 10)
+        cross_validation = cross_val_score(
+            estimator=estimator, X=train_X, y=train_y, cv=10
+        )
+        models_and_predictions[f"{estimator_name}"] = {
+            "prediction": prediction,
+            "confusion": confusion,
+            "accuracy": accuracy,
+            "avg_cvs": sum(cross_validation) / len(cross_validation),
+        }
 
 
 if __name__ == "__main__":
