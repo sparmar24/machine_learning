834db49f521f1b4edc39830b992d37d7117a50dc
Sonia Parmar <soniaparmar24@gmail.com>
2023-03-23 21:48:24 +0100

    Add for loop to calculate score

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -1,126 +1,123 @@
-# NLP model for sentiment analysis
+""" Natural Language Processing for restaurent reviews (sentiment analysis) """
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-from config import inputfile
-dataset = pd.read_csv(inputfile)
-   
-'''Data cleaning'''
-# import modules for deep cleaning the texts in dataset
 import re
-import nltk # used to ignore the words not useful, known as stopwords
-nltk.download("stopwords")
+import nltk  # used to ignore the words not useful i.e., stopwords
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-# removes all conjugations to make the reviews simple e.g loved to love
-
-# create an empty list
-corpus = []
-#starting with for loop to clean all 1000 reviews
-for i in range(0, 1000):
-    review = re.sub("[^a-zA-Z]", " ", dataset.iloc[:, 0][i]) # replace non letter by space
-    #OR review = re.sub("[^a-zA-Z]", " ", dataset["Review"][i])
-    review = review.lower()  # all letters to lower case
-    review = review.split()  # split the reviews in different columns
-
-    # apply stemming now
-    ps = PorterStemmer()
-    all_stopwards = stopwords.words("english")
-    all_stopwards.remove("not")
-    review = [ps.stem(word) for word in review if not word in set(all_stopwards)]
-    review = " ".join(review)
-    corpus.append(review)
-#print(corpus)
-
-'''Creating the Bag of Words model'''
-# tokenization process using scikit learn
 from sklearn.feature_extraction.text import CountVectorizer
-cv =  CountVectorizer(max_features = 1566)
-X = cv.fit_transform(corpus).toarray()  # put all the words in reviews into different columns
-y = dataset.iloc[:, -1].values
-#print(len(X[0]))
-
-'''splitting the dataset into train and test data sets'''
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state = 0)
-
-
-'''Naive Bayes'''
-print("Naive Bayes ---->")
+from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
-nb = GaussianNB()
-nb.fit(X_train, y_train)
-y_pred = nb.predict(X_test)
-#print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)),1))
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
-matrix = confusion_matrix(y_test, y_pred)
-print(matrix)
-score = accuracy_score(y_test, y_pred)
-print("score: ", score)
-pscore = precision_score(y_test, y_pred, average="binary")
-print("pscore: ", pscore)
-rscore = recall_score(y_test, y_pred, average="binary")
-print("rscore: ", rscore)
-f1score = f1_score(y_test, y_pred, average="binary")
-print("f1score: ", f1score)
-
-'''k-NN'''
-print("Knn ---->")
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
-y_pred_knn = knn.predict(X_test)
-from sklearn.metrics import confusion_matrix, accuracy_score
-matrix_knn = confusion_matrix(y_test, y_pred_knn)
-print(matrix_knn)
-score_knn = accuracy_score(y_test, y_pred_knn)
-print("score_knn: ", score_knn)
-pscore_knn = precision_score(y_test, y_pred_knn, average="binary")
-print("pscore_knn: ", pscore_knn)
-rscore_knn = recall_score(y_test, y_pred_knn, average="binary")
-print("rscore_knn: ", rscore_knn)
-f1score_knn = f1_score(y_test, y_pred_knn, average="binary")
-print("f1score_knn: ", f1score_knn)
-
-'''SVM'''
-print("SVM ---->")
 from sklearn.svm import SVC
-support = SVC(kernel="rbf", random_state=0)
-support.fit(X_train, y_train)
-y_pred_svm = support.predict(X_test)
-matrix_svm = confusion_matrix(y_test, y_pred_svm)
-print(matrix_svm)
-score_svm = accuracy_score(y_test, y_pred_svm)
-print("score_svm: ", score_svm)
-pscore_svm = precision_score(y_test, y_pred_svm, average="binary")
-print("pscore_svm: ", pscore_svm)
-rscore_svm = recall_score(y_test, y_pred_svm, average="binary")
-print("rscore_svm: ", rscore_svm)
-f1score_svm = f1_score(y_test, y_pred_svm, average="binary")
-print("f1score_svm: ", f1score_svm)
-
-
-'''Random Forest'''
-print("Random Forest ----> ")
 from sklearn.ensemble import RandomForestClassifier
-random = RandomForestClassifier(n_estimators=100, criterion="entropy",random_state=0)
-random.fit(X_train, y_train)
-y_pred_random = random.predict(X_test)
-matrix_random = confusion_matrix(y_test, y_pred_random)
-print(matrix_random)
-score_random = accuracy_score(y_test, y_pred_random)
-print("score_random: ", score_random)
-pscore_random = precision_score(y_test, y_pred_random, average="binary")
-print("pscore_random: ", pscore_random)
-rscore_random = recall_score(y_test, y_pred_random, average="binary")
-print("rscore_random: ", rscore_random)
-f1score_random = f1_score(y_test, y_pred_random, average="binary")
-print("f1score_random: ", f1score_random)
+from sklearn.metrics import (
+    confusion_matrix,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+)
+from xgboost import XGBClassifier
+from config import inputfile
 
 
-'''
+def pred_score(ps_estimator, ps_train_X, ps_test_X, ps_train_y, ps_test_y):
+    """predict score for each ML model"""
+    ps_estimator.fit(ps_train_X, ps_train_y)
+    prediction = ps_estimator.predict(ps_test_X)
+    confusion = confusion_matrix(ps_test_y, prediction)
+    accuracy = accuracy_score(ps_test_y, prediction)
+    precision = precision_score(ps_test_y, prediction, average="binary")
+    recall = recall_score(ps_test_y, prediction, average="binary")
+    f1score = f1_score(ps_test_y, prediction, average="binary")
+    return confusion, accuracy, precision, recall, f1score
+
+
+def main():
+    # dataset = pd.read_csv(inputfile)
+    dataset = pd.read_csv(
+        "~/PycharmProjects/MLProjects_UdemyCourse/part7_NaturalLanguageProcessing/Restaurant_Reviews.tsv",
+        delimiter="\t",
+        quoting=3,
+    )
+
+    """Data cleaning"""
+    # import modules for deep cleaning the texts in dataset
+    nltk.download("stopwords")
+    # removes all conjugations to make the reviews simple e.g loved to love
+
+    # create an empty list
+    corpus = []
+    # for loop to clean all the 1000 reviews
+    for i in range(0, 1000):
+        review = re.sub(
+            "[^a-zA-Z]", " ", dataset.iloc[:, 0][i]
+        )  # replace non letter by space
+        # OR review = re.sub("[^a-zA-Z]", " ", dataset["Review"][i])
+        review = review.lower()  # all letters to lower case
+        review = review.split()  # split the reviews in different columns
+
+        # apply stemming now
+        ps = PorterStemmer()
+        all_stopwards = stopwords.words("english")
+        all_stopwards.remove("not")
+        review = [
+            ps.stem(word) for word in review if not word in set(all_stopwards)
+        ]
+        review = " ".join(review)
+        corpus.append(review)
+        # print(corpus)
+
+    """ Creating the Bag of Words model"""
+    # tokenization process using scikit learn
+    cv = CountVectorizer(max_features=1566)
+    X = cv.fit_transform(
+        corpus
+    ).toarray()  # put all the words in reviews into different columns
+    y = dataset.iloc[:, -1].values
+    # print(len(X[0]))
+
+    """ splitting the dataset into train and test data """
+    train_X, test_X, train_y, test_y = train_test_split(
+        X, y, test_size=0.2, random_state=0
+    )
+
+    models_and_predictions = {}
+    estimators = {
+        "logistic": LogisticRegression(random_state=0),
+        "knn": KNeighborsClassifier(n_neighbors=5),
+        "svc_linear": SVC(kernel="linear", random_state=0),
+        "svc_rbf": SVC(kernel="rbf", random_state=0),
+        "gaussian_bayes": GaussianNB(),
+        "decision_tree": DecisionTreeClassifier(
+            criterion="entropy", random_state=0
+        ),
+        "random_forest": RandomForestClassifier(
+            criterion="entropy", n_estimators=5, random_state=0
+        ),
+        "xgboost": XGBClassifier(),
+    }
+
+    for estimator_name, estimator in estimators.items():
+        confusion, accuracy, precision, recall, f1score = pred_score(
+            estimator, train_X, test_X, train_y, test_y
+        )
+
+        models_and_predictions[f"{estimator_name}"] = {
+            "confusion": confusion,
+            "accuracy": accuracy,
+            "precision": precision,
+            "recall": recall,
+            "f1score": f1score,
+        }
+        
+
+"""
  #Bonus class: Predicting a single review if positive or negative
 textlist = []
 #text = "I love this restaurent so much"
@@ -143,4 +140,8 @@ print(textlist)
 
 new_X_test = cv.transform(textlist).toarray()
 new_y_pred = random.predict(new_X_test) # can test the prediction with different models
-print(new_y_pred)'''
+print(new_y_pred)"""
+
+
+if __name__ == "__main__":
+    main()
